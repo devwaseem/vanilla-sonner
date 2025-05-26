ab3d02bc2f57deaa8234e5f76f4d41b7da8e19f6
Waseem Akram <waseem07799@gmail.com>
2025-05-26 13:50:42 +0530

    chore(vite): use esbuild, disable css code split

--- vite.config.mjs
+++ vite.config.mjs
@@ -9,19 +9,7 @@ export default defineConfig({
       fileName: (format) => `sonner.${format}.min.js`,
       formats: ["es", "umd"],
     },
-    minify: "terser",
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true,
-        passes: 3,
-      },
-      mangle: true,
-      format: {
-        comments: false,
-      },
-    },
-    cssCodeSplit: true,
+    minify: true,
     rollupOptions: {
       output: {
         exports: "named",
