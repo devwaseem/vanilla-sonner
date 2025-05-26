18e95f1168e6044648348697f00e5af5de9f2d14
Waseem Akram <waseem07799@gmail.com>
2025-05-26 13:33:32 +0530

    chore(vite): drop debugger in terser, split css from js, use cssnano for css minification

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
+import cssnano from "cssnano";
 
 export default defineConfig({
   build: {
     lib: {
-      entry: "src/index.ts",
+      entry: "src/sonner.ts",
       name: "Sonner",
       fileName: (format) => `sonner.${format}.min.js`,
       formats: ["es", "umd"],
@@ -12,17 +13,20 @@ export default defineConfig({
     terserOptions: {
       compress: {
         drop_console: true,
+        drop_debugger: true,
+        passes: 3,
       },
       mangle: true,
       format: {
         comments: false,
       },
     },
+    cssCodeSplit: true,
     rollupOptions: {
       output: {
         exports: "named",
       },
     },
   },
-  plugins: [],
+  plugins: [cssnano({ preset: "default" })],
 });
